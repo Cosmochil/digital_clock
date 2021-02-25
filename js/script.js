94180d8c942b7e2ed272316c5a1e252391898d61
Cosmochil <tochiruchiru@gmail.com>
2021-02-25 22:00:49 +0700

    clock with theme

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-/*
-fine then lets focus on our clock
-yes
-can you see the live server?
-Yes I can see the live server
-Come to the function time_right_now
-*/
-let current_time = new Date();
 let current_date = document.querySelector(`#date`);
 let current_day = document.querySelector(`#day`);
 let clock = document.querySelector(`#clock`);
 
-// I have an idea
-// uhuh Come below
-function date_right_now(date){
+
+function date_right_now(){
+    let date = new Date();
+
     let date_today = date.getDate()
-    let month_today = date.getMonth() + 1;
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Des"];
+    let month_today = months[date.getMonth()];
     let year_today = date.getFullYear();
-    
     current_date.innerHTML = `
         <main> 
-            <h1>${date_today} ${month_today} ${year_today}</h1>
+            <h1 class ="date" >${date_today} ${month_today} ${year_today}</h1>
         </main>
     `
 }
 
 
-function day_right_now(date){
+function day_right_now(){
+
+    let date = new Date();
+
     let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     let today = week[date.getDay()];
+
     current_day.innerHTML =`
-        <h2>${today}</h2>
+        <h2 class="day">${today}</h2>
     `
 }
-// freeze or thinking? I'm thinking
-function time_right_now(time){
+//
+function time_right_now(){
+
+    let time = new Date();
+
     // hours
     let hours = time.getHours() % 12;
     hours = hours ? hours : 12; 
@@ -44,12 +44,12 @@ function time_right_now(time){
     // seconds
     let seconds = padding(time.getSeconds());
        
-    let period = hours >= 12 ? 'PM' : 'AM';
+    let period = hours >= 12 ? 'AM' : 'PM';
 
     let clock_time = `${hours}:${minutes}:${seconds} ${period}`;
 
     clock.innerHTML = ` 
-        <h2>${clock_time}</h2>
+        <h2 class="clock">${clock_time}</h2>
     `
 }
 
@@ -57,16 +57,24 @@ function padding(value) {
     return value < 10 ? "0" + value : value;
 }
 
+function main() {
+    date_right_now();
+    day_right_now();
+    setInterval(() => time_right_now(),1000);
+}
 
-setInterval(() => time_right_now(current_time),1000);
+main();
 
-date_right_now(current_time);
-day_right_now(current_time);
+// Dark mode toggle switch
 
+const toggle_switch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-/*
-okay I have an idea. 
-I think this is not working in live server
-Try to push this project online and then share the link with me
-I hope that works. 
-*/
+function switch_theme(event){
+    if(event.target.checked){
+        document.documentElement.setAttribute('dark-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('dark-theme', 'light');
+    }
+}
+
+toggle_switch.addEventListener('change', switch_theme, false)
